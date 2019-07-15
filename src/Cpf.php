e248031123809d0aa241b6e3f408bff0663453f8
Adelar Tiemann Junior <adelar@adelarcubs.com>
2019-07-15 14:46:06 -0300

    Mudando para AbstractValidator

--- src/Cpf.php
+++ src/Cpf.php
@@ -1,9 +1,10 @@
 <?php
+declare(strict_types = 1);
 namespace CpfCnpjValidation;
 
-use Zend\Validator\ValidatorInterface;
+use Zend\Validator\AbstractValidator;
 
-class Cpf implements ValidatorInterface
+class Cpf extends AbstractValidator
 {
 
     private $invalidCpf = [
@@ -19,35 +20,24 @@ class Cpf implements ValidatorInterface
         '99999999999'
     ];
 
-    /**
-     * Returns true if and only if $value meets the validation requirements
-     *
-     * If $value fails validation, then this method returns false, and
-     * getMessages() will return an array of messages that explain why the
-     * validation failed.
-     *
-     * @param mixed $value            
-     * @return bool
-     * @throws Exception\RuntimeException If validation of $value is impossible
-     */
     public function isValid($value)
     {
         if (empty($value)) {
             return false;
         }
-        
+
         // Elimina possivel mascara
         $cpf = preg_replace('/[^0-9]/', '', (string) $value);
-        
+
         if (strlen($cpf) > 11) {
             return false;
         }
         $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
-        
+
         if (in_array($cpf, $this->invalidCpf)) {
             return false;
         }
-        
+
         for ($t = 9; $t < 11; $t ++) {
             for ($d = 0, $c = 0; $c < $t; $c ++) {
                 $d += $cpf{$c} * (($t + 1) - $c);
@@ -58,21 +48,5 @@ class Cpf implements ValidatorInterface
             }
         }
         return true;
-    }
-
-    /**
-     * Returns an array of messages that explain why the most recent isValid()
-     * call returned false.
-     * The array keys are validation failure message identifiers,
-     * and the array values are the corresponding human-readable message strings.
-     *
-     * If isValid() was never called or if the most recent isValid() call
-     * returned true, then this method returns an empty array.
-     *
-     * @return array
-     */
-    public function getMessages()
-    {
-        return [];
     }
 }
