cdfbe9e8da5432fe8101f74d3f40974437c30829
Edmar Gomes <edmar.gomes@nyx.tc>
2021-07-13 10:14:47 -0300

    Corrigindo o erro 'Array and string offset access syntax with curly braces is deprecated'.

--- src/Cnpj.php
+++ src/Cnpj.php
@@ -20,19 +20,19 @@ class Cnpj extends AbstractValidator
         $cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
         // Valida primeiro dígito verificador
         for ($i = 0, $j = 5, $soma = 0; $i < 12; $i ++) {
-            $soma += $cnpj{$i} * $j;
+            $soma += $cnpj[$i] * $j;
             $j = ($j == 2) ? 9 : $j - 1;
         }
         $resto = $soma % 11;
-        if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto)) {
+        if ($cnpj[12] != ($resto < 2 ? 0 : 11 - $resto)) {
             return false;
         }
         // Valida segundo dígito verificador
         for ($i = 0, $j = 6, $soma = 0; $i < 13; $i ++) {
-            $soma += $cnpj{$i} * $j;
+            $soma += $cnpj[$i] * $j;
             $j = ($j == 2) ? 9 : $j - 1;
         }
         $resto = $soma % 11;
-        return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
+        return $cnpj[13] == ($resto < 2 ? 0 : 11 - $resto);
     }
 }
